ed144968166b78de077f4eacbe7bb71d8a26686f
Christinagithub <40464298+Christinagithub@users.noreply.github.com>
2019-06-28 22:37:35 +0800

    覆盖地图
    重新改的代码

---
+++ GPS/src/org/apache/log4j/Logger.java
@@ -0,0 +1,17 @@
+package org.apache.log4j;
+
+import com.drew.metadata.exif.AppSendUtils;
+
+public class Logger {
+
+	public static Logger getLogger(Class<AppSendUtils> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void error(Exception e, Exception e2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
